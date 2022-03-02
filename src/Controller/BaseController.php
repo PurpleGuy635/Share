87914ba462a91d20a9d8d70077f51fa2e13ffd3c
Francois <admin@purpleguy.ovh>
2022-03-02 17:46:11 +0100

    share v1

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    #[Route('/index', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+            'controller_name' => 'BaseController', // suppression de la variable passée à la vue
+        ]);
+
+    }
+
+    #[Route('/contact', name: 'contact')] // étape 1
+    public function contact(): Response // étape 2
+    {
+        return $this->render('base/contact.html.twig', [ // étape 3
+            
+        ]);
+    }
+
+    #[Route('/mention', name: 'mention')] // étape 1
+    public function mention(): Response // étape 2
+    {
+        return $this->render('base/mention.html.twig', [ // étape 3
+            
+        ]);
+    }
+}
+      
